170d2a6582a84b8e36b25c904175ce67b3a86873
leosantosw <leonardosantosx0077@gmail.com>
2025-02-16 10:43:23 -0300

    feat: implement methods in savings account

--- src/bank/accounts/savingsAccount.go
+++ src/bank/accounts/savingsAccount.go
@@ -5,5 +5,28 @@ import "github.com/bank/customers"
 type SavingsAccount struct {
 	Customer                               customers.Customer
 	AgencyNumber, AccountNumber, Operation int
-	Balance                                float64
+	balance                                float64
+}
+
+func (c *SavingsAccount) Withdraw(amount float64) (string, float64) {
+	canWithdraw := amount > 0 && amount <= c.balance
+	if canWithdraw {
+		c.balance -= amount
+		return "successfully", c.balance
+	} else {
+		return "enough balance", c.balance
+	}
+}
+
+func (c *SavingsAccount) Deposit(amount float64) (string, float64) {
+	if amount > 0 {
+		c.balance += amount
+		return "successfully", c.balance
+	} else {
+		return "enough deposit", c.balance
+	}
+}
+
+func (c *SavingsAccount) GetBalance() float64 {
+	return c.balance
 }
